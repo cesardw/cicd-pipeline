aaeb8a84756f8604ebf120cd7e69caef8f4bed5a
Cesar Delgado <cesards91@gmail.com>
2023-10-27 14:13:18 -0600

    cambio

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "DevOps en proceso, sale"
+        message: "DevOps en proceso, sale o sale"
     })
 })
 
