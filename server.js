b00bd54c9bd52b621566e667b9d2a1177edfb068
Cesar Delgado <cesards91@gmail.com>
2024-01-19 20:47:22 -0600

    ultima prueba

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "DevOps en proceso, sale o sale 3.0"
+        message: "Toca SSH"
     })
 })
 
